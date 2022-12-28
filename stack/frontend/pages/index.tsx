51f3547b7356fd296512e0fa8ec97744baef32a0
FreddyWordingham <freddy@digilab.co.uk>
2022-12-28 18:52:18 +0000

    Updated assets

--- stack/frontend/pages/index.tsx
+++ stack/frontend/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
                 <title>Freddy | Software Projects </title>
                 <meta name="description" content="Software experiments" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon_circ.ico" />
             </Head>
             <main>
                 <h1>
