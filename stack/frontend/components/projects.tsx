9a2594f5baa6aebe70f8feec44b9f1de31f2577e
FreddyWordingham <freddy@digilab.co.uk>
2022-12-29 21:35:44 +0000

    Added project item placeholder content

--- stack/frontend/components/projects.tsx
+++ stack/frontend/components/projects.tsx
@@ -11,24 +11,24 @@ const Projects = () => {
                 </span>
                 <h2 className="py-4">Things I've built</h2>
                 <div className="grid md:grid-cols-2 gap-8">
-                    <ProjectItem />
-                    <ProjectItem />
-                    <ProjectItem />
-                    <ProjectItem />
+                    <ProjectItem title="Placeholder: Crypto" backgroundImg={"/assets/projects/crypto.jpg"} projectUrl="/crypto" />
+                    <ProjectItem title="Placeholder: Netflix" backgroundImg={"/assets/projects/netflix.jpg"} projectUrl="/crypto" />
+                    <ProjectItem title="Placeholder: Property" backgroundImg={"/assets/projects/property.jpg"} projectUrl="/crypto" />
+                    <ProjectItem title="Placeholder: Twitch" backgroundImg={"/assets/projects/twitch.jpg"} projectUrl="/crypto" />
                 </div>
             </div>
         </div>
     );
 };
 
-const ProjectItem = () => {
+const ProjectItem = ({ title, backgroundImg, projectUrl }: { title: string; backgroundImg: string; projectUrl: string }) => {
     return (
         <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
-            <Image className="rounded-xl group-hover:opacity-50" src="/assets/projects/crypto.jpg" alt="Crypto" width={512} height={256} />
+            <Image className="rounded-xl group-hover:opacity-20" src={backgroundImg} alt="Crypto" width={512} height={256} />
             <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-                <h3 className="text-2xl text-white tracking-wider text-center">Crypto</h3>
+                <h3 className="text-2xl text-white tracking-wider text-center">{title}</h3>
                 <p className="pb-4 pt-2 text-white text-center">React JS</p>
-                <Link href="/">
+                <Link href={projectUrl}>
                     <p className="text-center p-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">More info</p>
                 </Link>
             </div>
