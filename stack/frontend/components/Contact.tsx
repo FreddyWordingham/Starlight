64aabcbf70e79435d56f93dabc8c99a5e932ed98
FreddyWordingham <freddy@digilab.co.uk>
2022-12-30 15:09:55 +0000

    Added contact form

--- stack/frontend/components/Contact.tsx
+++ stack/frontend/components/Contact.tsx
@@ -12,10 +12,10 @@ const Contact = () => {
                 </span>
                 <h2 className="py-4">Get in touch</h2>
                 <div className="grid lg:grid-cols-5 gap-8">
-                    <div className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+                    <div className="col-span-3 lg:col-span-2 w-full h-auto shadow-xl shadow-gray-400 rounded-xl p-4">
                         <Description />
                     </div>
-                    <div className="col-span-3 lg:col-span-3 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+                    <div className="col-span-3 lg:col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl p-4">
                         <ContactForm />
                     </div>
                 </div>
@@ -46,7 +46,34 @@ const Description = () => {
 };
 
 const ContactForm = () => {
-    return <div className="lg:p-4 h-full">Form</div>;
+    return (
+        <div className="p-4">
+            <form>
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                    <div className="flex flex-col">
+                        <label className="uppercase text-sm py-2">Name</label>
+                        <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="uppercase text-sm py-2">Phone Number</label>
+                        <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" />
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2">Email</label>
+                    <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" />
+                </div>
+                <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2">Subject</label>
+                    <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" />
+                </div>
+                <div className="flex flex-col">
+                    <label className="uppercase text-sm py-2">Message</label>
+                    <textarea className="border-2 rounded-lg p-3 border-gray-300" rows="11"></textarea>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default Contact;
