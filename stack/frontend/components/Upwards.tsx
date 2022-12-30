a6ef9b96020d798aa043cda4d10f88a826a5d6f0
FreddyWordingham <freddy@digilab.co.uk>
2022-12-30 15:24:09 +0000

    Added upwards styling

--- stack/frontend/components/Upwards.tsx
+++ stack/frontend/components/Upwards.tsx
@@ -7,7 +7,7 @@ const Upwards = () => {
         <div className="flex justify-center py-12">
             <Link href="/">
                 <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <HiOutlineChevronDoubleUp size={40} />
+                    <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={40} />
                 </div>
             </Link>
         </div>
